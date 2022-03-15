9d74e931dd6d130999aa6f5bc17a2990dd2624f0
joarwar <joarwar@gmail.com>
2022-03-15 22:15:47 +0100

    Menu started

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 Vue.createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            monthInput: '',
+            yearInput: ''
+
+        }
+    },
+    methods:{
+        async saveInput(){
+            let month = monthInput;
+            let year = yearInput;
+            
         }
     }
-}).mount('#app')
+}).mount('main')
+
+
