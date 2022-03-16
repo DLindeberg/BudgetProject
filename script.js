243c75542e83595b327a6493c55f95103bb01ec2
DLindeberg <90319103+DLindeberg@users.noreply.github.com>
2022-03-16 21:07:34 +0100

    Refactored html options into lists

--- script.js
+++ script.js
@@ -2,15 +2,37 @@ Vue.createApp({
     data() {
         return {
             monthInput: '',
-            yearInput: ''
+            monthOptions: [
+                { text: 'January', value: 'jan'},
+                { text: 'February', value: 'feb'},
+                { text: 'March', value: 'mar'},
+                { text: 'April', value: 'apr'},
+                { text: 'May', value: 'may'},
+                { text: 'June', value: 'jun'},
+                { text: 'July', value: 'jul'},
+                { text: 'August', value: 'aug'},
+                { text: 'September', value: 'sep'},
+                { text: 'October', value: 'oct'},
+                { text: 'November', value: 'nov'},
+                { text: 'December', value: 'dec'},
+            ],
+            yearInput: '',
+            yearOptions: [
+                { text: '2022', value: 2022},
+                { text: '2023', value: 2023},
+                { text: '2024', value: 2024},
+                { text: '2025', value: 2025},
+                { text: '2026', value: 2026},
+            ],
+            yearList: []
 
         }
     },
     methods:{
-        async saveInput(){
+        saveInput(){
             let month = monthInput;
             let year = yearInput;
-            
+            alert('hej')
         }
     }
 }).mount('main')
