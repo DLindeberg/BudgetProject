3edec09ad8950610f8fb5c9ec5d6612bc79ab089
DLindeberg <90319103+DLindeberg@users.noreply.github.com>
2022-03-17 16:31:14 +0100

    Added additional json

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
 Vue.createApp({
     data() {
         return {
-            monthInput: '',
-            monthOptions: [
-                { text: 'January', value: 'jan'},
-                { text: 'February', value: 'feb'},
-                { text: 'March', value: 'mar'},
-                { text: 'April', value: 'apr'},
-                { text: 'May', value: 'may'},
-                { text: 'June', value: 'jun'},
-                { text: 'July', value: 'jul'},
-                { text: 'August', value: 'aug'},
-                { text: 'September', value: 'sep'},
-                { text: 'October', value: 'oct'},
-                { text: 'November', value: 'nov'},
-                { text: 'December', value: 'dec'},
-            ],
             yearInput: '',
-            yearOptions: [
-                { text: '2022', value: 2022},
-                { text: '2023', value: 2023},
-                { text: '2024', value: 2024},
-                { text: '2025', value: 2025},
-                { text: '2026', value: 2026},
+            monthInput: '',
+            years: [
+                { text: '2022', value: 2022 },
+                { text: '2023', value: 2023 },
+                { text: '2024', value: 2024 },
+                { text: '2025', value: 2025 },
+                { text: '2026', value: 2026 },
             ],
-            yearList: []
-
+            months: [],
+            categories: [],
         }
     },
-    methods:{
-        saveInput(){
-            let month = this.monthOptions.value;
-            alert(month)
+    methods: {
+        saveInput() {
         }
+    },
+    mounted: async function () {
+        let response = await fetch('months.json');
+        let response2 = await fetch('categories.json');
+        let json = await response.json();
+        let json2 = await response2.json();
+        this.months = json;
+        this.categories = json2;
     }
 }).mount('main')
-
-
