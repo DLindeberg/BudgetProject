81c0bb30c1e30ab0d17808b465e422ab355d614b
DLindeberg <90319103+DLindeberg@users.noreply.github.com>
2022-03-19 22:54:25 +0100

    alternative data

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ Vue.createApp({
             income: '',
             savedCategories: [],
             newCat: [],
+            data: [],
         }
     },
     methods:{
@@ -36,10 +37,13 @@ Vue.createApp({
     mounted: async function () {
         let response = await fetch('months.json');
         let response2 = await fetch('categories.json');
+        let response3 = await fetch('data.json');
         let json = await response.json();
         let json2 = await response2.json();
+        let json3 = await response3.json();
         this.months = json;
         this.categories = json2;
+        this.data = json3;
     },
 
 }).mount('main')
